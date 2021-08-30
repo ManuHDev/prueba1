a0ea4c5dc7b0cd6e58e7522a07da20b35c464274
ManuelHamana <mhamana@strappinc.net>
2021-08-29 20:24:41 -0400

    commit

---
+++ index.php
@@ -0,0 +1,87 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>DW Latam</title>
+    <link href="css/main.css" rel="stylesheet">
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap2.css" rel="stylesheet">
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+    <script src="css/ie-emulation-modes-warning.js"></script>
+    <link href="css/carousel.css" rel="stylesheet">
+    <link href="imagenes/logo.png" rel="shortcut icon" type="image/x-icon" />
+    <!-- para cambiar la sombre en el menu-->
+    <script src="css/menu.js">	</script>
+    
+    
+    <!--Scroll-->
+    <!--script src="vista/js/jquery-1.11.3.min.js"></script-->
+    <script src="vista/js/jquery.nav.js"></script>
+    <script src="vista/js/custom.js"></script>
+    
+    <!--Slider-->
+    <link href="css/nivo-slider.css" rel="stylesheet" type="text/css" />
+    <script src="script/JQuery-1.4.3.js" type="text/javascript"></script> 
+	<script src="script/jquery.nivo.slider.pack.js" type="text/javascript"></script>   
+    <script type="text/javascript">
+            $(window).load(function () {//alert('aqui');
+                $('#slider').nivoSlider();				
+            });
+    </script>
+	<script src="script/tCorreo.js" type="text/javascript"></script>
+    <!--Generales-->
+   	<script type="text/javascript" src="script/crossBrowser.js"> </script>
+    <script type="text/javascript" src="script/documentObjectModel.js"> </script>
+    <script type="text/javascript" src="script/ajaxRequest.js"></script>
+    <script type="text/javascript" src="script/probando.js"></script>
+    <script type="text/javascript" src="script/general.js"></script>
+    <!--script type="text/javascript" src="script/Scroll.js"></script-->
+  </head>
+<!-- NAVBAR ================================================== -->
+<body>
+
+<?php
+include("menu.php");
+echo "<div id='inicio'></div>";
+//include("carousel.php");
+echo "<br>";
+echo "<br>";
+echo "<br>";
+include("slider2.html");
+//<!-- START THE FEATURETTES -->
+?>
+<br>
+<div  class="container-fluid marketing">
+    <div class="row">
+        <div class="col-lg-1">
+        </div>
+        <div class="col-lg-10">
+	    	<h4 align="center">"Somos un grupo de profesionales emprendedores, proactivos y con experiencia en el desarrollo de soluciones informáticas. Nacimos por la necesidad existente en el mercado de ofrecer no solo herramientas, sino soluciones y servicios  a nivel nacional e internacional, ofreciéndoles a nuestros clientes ser su aliado estratégico para apoyarlos en el logro de sus objetivos, mejorar su productividad e incrementar su rentabilidad."</h4>
+        </div>
+        <div class="col-lg-1">
+        </div>
+	</div><!-- Row -->
+</div><!-- Container -->
+<?php
+include("servicios.php");
+include("contacto.php");
+include("comentario.php");
+include("pie.php");
+?>
+
+    <!-- Bootstrap core JavaScript ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <!--script src="css/jquery.js"></script-->
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="css/bootstrap.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="css/holder.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="css/ie10-viewport-bug-workaround.js"></script>
+<svg style="display: none; visibility: hidden; position: absolute; top: -100%; left: -100%;" preserveAspectRatio="none" viewBox="0 0 500 500" height="500" width="500" xmlns="http://www.w3.org/2000/svg"><defs><style type="text/css"></style></defs><text style="font-weight:bold;font-size:25pt;font-family:Arial, Helvetica, Open Sans, sans-serif" y="25" x="0">500x500</text></svg></body></html>
